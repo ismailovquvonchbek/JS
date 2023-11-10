8cd6137c9e32fee882c01caae7ccf8b7b27187d5
ismailovquvonchbek <ismailovquvonchbek08@gmail.com>
2023-11-10 20:33:22 +0500

    Savol javob

---
+++ js/main.js
@@ -0,0 +1,47 @@
+var Name = String(prompt("Ismingizni yozing..."))
+console.log(Name);
+
+var SurName = String(prompt("Familyangizni yozing..."))
+console.log(SurName);
+
+var FISH = String(prompt("Atchestva yozing..."))
+console.log(FISH);
+
+var Salom = String(prompt("Zor narsa yozing..."))
+console.log(Salom);
+
+var Current = String(prompt("Daxshat narsa yozing 😂"))
+console.log(Current);
+
+var Doc = String(prompt("Baxtli kuningizni gaprib bering ozimga 😉"))
+console.log(Doc);
+
+var Data = Number(prompt("Tugilgan kun qachon"))
+console.log(Data);
+
+
+var Malumot = String(prompt("Storm akademy qachondan endi 😃"))
+console.log(Malumot);
+
+var Type = String(prompt("Men bilan bir kun otkazgan bolarmidingiz"))
+console.log(Type);
+
+var Info = String(prompt("Qiz topish kere menga taklif..."))
+console.log(Info);
+
+
+var Javob = [
+    "Ism: " + " " + Name + "\n" +
+    "Familya: " + " " + SurName + "\n" +
+    "Otasini ismi: " + " " + FISH + "\n" +
+    "Izoh: " + " " + Salom + "\n" +
+    "Zo'r: " + " " + Current + "\n" +
+    'Yozing: ' + " " + Doc + "\n" +
+    'Tugilgan kun: ' + " " + Data + "\n" +
+    'Kurs sanasi: ' + " " + Malumot + "\n" +
+    "Masalanda: " + " " + Type + "\n" +
+    "Qizlar ismlari : " + " " + Info + "\n" 
+]
+
+
+alert(Javob)
